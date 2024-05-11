18ca423919beae834f09131b0ceff86c81373fea
JSv4 <scrudato@umich.edu>
2024-05-10 20:35:31 -0500

    PAWLS tokens and OpenContract format seems to be working pretty nicely.

--- nlm_ingestor/datatypes/dicts.py
+++ nlm_ingestor/datatypes/dicts.py
@@ -1,4 +1,4 @@
-from typing import TypedDict
+from typing import TypedDict, Union, Optional
 
 
 class PawlsPageBoundaryPythonType(TypedDict):
@@ -9,6 +9,7 @@ class PawlsPageBoundaryPythonType(TypedDict):
     width: float
     height: float
     index: int
+
 
 class PawlsTokenPythonType(TypedDict):
     """
@@ -32,3 +33,51 @@ class PawlsPagePythonType(TypedDict):
     page: PawlsPageBoundaryPythonType
     tokens: list[PawlsTokenPythonType]
 
+
+class BoundingBoxPythonType(TypedDict):
+    """
+    Bounding box for pdf box on a pdf page
+    """
+
+    top: int
+    bottom: int
+    left: int
+    right: int
+
+
+class TokenIdPythonType(TypedDict):
+    """
+    These are how tokens are referenced in annotation jsons.
+    """
+
+    pageIndex: int
+    tokenIndex: int
+
+
+class OpenContractsSinglePageAnnotationType(TypedDict):
+    """
+    This is the data shape for our actual annotations on a given page of a pdf.
+    In practice, annotations are always assumed to be multi-page, and this means
+    our annotation jsons are stored as a dict map of page #s to the annotation data:
+
+    Dict[int, OpenContractsSinglePageAnnotationType]
+
+    """
+
+    bounds: BoundingBoxPythonType
+    tokensJsons: list[TokenIdPythonType]
+    rawText: str
+
+
+class OpenContractsAnnotationPythonType(TypedDict):
+    """
+    Data type for individual Open Contract annotation data type converted
+    into JSON. Note the models have a number of additional fields that are not
+    relevant for import/export purposes.
+    """
+
+    id: Optional[Union[str, int]]  # noqa  # fmt: off
+    annotationLabel: str
+    rawText: str
+    page: int
+    annotation_json: dict[Union[int, str], OpenContractsSinglePageAnnotationType]
